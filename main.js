7c163b0e3692ba536289a624c03359580926273f
YeimerSebastianRomeroGilADSO <chattian369@gmail.com>
2022-10-18 17:22:36 -0500

    punto 2

--- main.js
+++ main.js
@@ -1,14 +1,20 @@
 addEventListener("DOMContentLoaded", ()=>{
-    let x1 = Number(prompt(" ingrese el valor de x1 "))
-    let x2 = Number(prompt(" ingrese el valor de x2 "))
-    let y1 = Number(prompt(" ingrese el valor de y1 "))
-    let y2 = Number(prompt(" ingrese el valor de y2 "))
-    x = Math.pow ( x2-x1,2)
-    y = Math.pow( y2-y1, 2)
-    r = Math.sqrt ( x+y)
-    let  resta = (r<0)
-
-               ? "¡¡ la solucion es indefinidad !!"
-               : ` la distacia es ${r}`
-    console.log(` el valor de la distancia es : ${r} `)
+    let  tot = 0
+    let des = 0
+    let step = 0
+    for (step = 0; step < 3; step ++){
+        let p1 = Number( prompt( "cual fue el costo del producto ", 2300))
+        c1 = p1-( p1*5/100)
+        i1 = p1 - c1 
+        tot = c1 + tot
+        des = i1 +des 
+    }
+    for (step = 0; step < 2; step ++){
+        let p1 = Number( prompt( "cual fue el costo del producto ", 2300))
+        c1 = p1-( p1*2/100)
+        i1 = p1 - c1 
+        tot = c1 + tot
+        des = i1 +des 
+    }
+ console.log(`el costo de sus productos con el descuento es ${tot}$, su descuento es de ${des},$`)
 })
