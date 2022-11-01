df05b24cbb424935a6288c4872ba2cdb473b33fc
YeimerSebastianRomeroGilADSO <chattian369@gmail.com>
2022-11-01 10:24:44 -0500

    punto 4 js

--- main.js
+++ main.js
@@ -1,12 +1,19 @@
 addEventListener("DOMContentLoaded", ()=>{
-    let altu = Number(prompt(" ingrese la altura "))
-    let radio = Number ( prompt(" ingrese el radio "))
-    let caudal = Number(prompt(" ingrese el caudal "))
-    vl = Math.PI*(radio**2) * altu
-    j = vl / caudal
-    tiempo = j / 60
-document.write(`EL TIEMPO EN LLENAR ES: ${tiempo}  <br>`)
+    let q = Number(prompt("Cual es el caudal aproximado en (litros/segundo)? ", 210))
+    let h = Number(prompt("Cual es la altura del deposito? ", 100))
+    let r = Number(prompt("Cual es el radio del deposito? ", 200))
+    v = (Math.PI) * Math.pow(r, 2) * h
+    t = v / q
+    m = t / 60
+    document.body.insertAdjacentHTML("beforeend", `<div class="estilo">El tiempo en llenar su deposito es ${t} <br> El tiempo que demoraria en llecarse es ${m} </div> `);
+    let llaves = 11500 * 5
+    let bomba=1168000
+    let pernos=87000*3
+    let vueltos=91000
+    total=llaves+bomba+pernos+vueltos
+    total1=llaves+bomba+pernos
 
+console.log(`El total que pago por los productos:<b> ${total1}$</b><br>Entonces sabemos que traia: <b>${total}$`)  
 
 
 
