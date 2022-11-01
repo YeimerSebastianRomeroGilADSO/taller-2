e463a08decf5d59734070c7ef089741d86056e9c
YeimerSebastianRomeroGilADSO <chattian369@gmail.com>
2022-11-01 10:37:46 -0500

    punto 8

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 addEventListener("DOMContentLoaded", ()=>{
-    let cjs = Number(prompt("Cuantas cajas de repuestos transporta? "))
-    total = cjs * 748
-    console.log(` El peso que transporta es de ${total} kg`)
-
+    let m2 = Number(prompt("Cantidad de m²"))
+    let valor = Number(prompt("Precio del m²?"))
+    total = m2 * valor
+console.log(` El valor de su pintura es ${total}$`)
 
 })
