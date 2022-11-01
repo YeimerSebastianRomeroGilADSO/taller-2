602ca3eb7b50ce136dc55526fc5aec40506e99cd
YeimerSebastianRomeroGilADSO <chattian369@gmail.com>
2022-11-01 10:41:23 -0500

    punto 10

--- main.js
+++ main.js
@@ -1,8 +1,4 @@
 addEventListener("DOMContentLoaded", ()=>{
-    let m2 = Number(prompt("Cantidad de m²"))
-    let valor = Number(prompt("Precio del m²?"))
-    total = m2 * valor
-    document.body.insertAdjacentHTML("beforeend", `<div class="estilo">El valor de su pintura es ${total}$</div> `);
     let nombre = prompt("Ingrese el nombre del personal: ")
     let apellido = prompt("Ingrese apellido del personal")
     let año = Number(prompt("Ingrese el año de nacimiento: "))
