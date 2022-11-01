4931902023df98112152861242386b0226603570
YeimerSebastianRomeroGilADSO <chattian369@gmail.com>
2022-11-01 10:33:12 -0500

    punto  6

--- main.js
+++ main.js
@@ -1,12 +1,7 @@
 addEventListener("DOMContentLoaded", ()=>{
-    let llaves = 11500 * 5
-    let bomba=1168000
-    let pernos=87000*3
-    let vueltos=91000
-    total=llaves+bomba+pernos+vueltos
-    total1=llaves+bomba+pernos
-console.log(`El total que pago por los productos:${total1}$ Entonces sabemos que traia: ${total}$`)
-
+    let cajas = Number(prompt("Cuantas cajas de repuestos transporta? "))
+    total = cajas * 748
+    console.log(`El peso que transporta es de ${total} kg`)
 
 
 })
